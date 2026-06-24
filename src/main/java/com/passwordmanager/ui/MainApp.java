0d58e12fb5ae246b7f5f5e0becc3c351633ce357
GoodDuck558 <defuse-pound-mug@duck.com>
2026-06-24 14:01:03 +0300

    Zero masterPassword on app close

--- src/main/java/com/passwordmanager/ui/MainApp.java
+++ src/main/java/com/passwordmanager/ui/MainApp.java
@@ -5,19 +5,29 @@ import com.formdev.flatlaf.FlatLightLaf;
 import javax.swing.*;
 
 public class MainApp {
-
     public static void main(String[] args) {
-
         try {
             FlatLightLaf.setup();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Password Manager");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(800, 650);
             frame.setResizable(false);
+
+            frame.addWindowListener(new java.awt.event.WindowAdapter() {
+                @Override
+                public void windowClosing(java.awt.event.WindowEvent e) {
+                    if (AppState.masterPassword != null) {
+                        for (int i = 0; i < AppState.masterPassword.length; i++) {
+                            AppState.masterPassword[i] = '\0';
+                        }
+                    }
+                }
+            });
 
             MasterPasswordScreen screen = new MasterPasswordScreen(frame);
             frame.setContentPane(screen.getPanel());
